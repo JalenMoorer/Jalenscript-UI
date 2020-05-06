f9a4bf1fd1ad08e69c07e8436cd0b23e281ddc01
JALEN <jalenmmoorer@gmail.com>
2020-05-06 14:38:44 -0400

    Added Header Component to Storybook

---
+++ src/components/Header/Header.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import styles from './Header.module.css';
+import logo from './images/jalenscript-logo.png';
+
+function Header(props) {
+
+let buttonChild;
+
+    if (!props.mobileNav) {
+        buttonChild = (
+            <button
+            type="button"
+            className={styles.hamburgerMenu} 
+            aria-label="Open Navigation Menu"
+            aria-controls="mobileNavMenu"
+            id="hamburgerMenu"
+            onClick={props.onOpenMobileNav}
+        >
+            <div></div>
+            <div></div>
+            <div></div>
+        </button>
+        )
+    }
+    else {
+    buttonChild = ( 
+        <button
+            type="button"
+            className={styles.closeMobileButton} 
+            aria-label="Close Navigation Menu"
+            aria-Controls=""
+            id="closeMobileNavigation"
+            onClick={props.oncloseMobileNav}
+        >
+            <div></div>
+        </button>
+        )
+    }
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.logoArea}>
+            <div className={styles.logo}>
+                <a href="/" alt="JalenScript"><img alt="JalenScript" src={logo}/></a>  
+            </div>
+            <div className={styles.navigationArea} >
+                    <nav className={styles.navigation}>
+                        <div className={styles.hideNavButton}>
+                            {buttonChild}
+                        </div>
+                        <ul className={styles.hideNav}>
+                            <li><a onClick={props.scrollToTargetAdjusted} href="#About">About me</a></li>
+                            <li><a onClick={props.scrollToTargetAdjusted} href="#Skills">Skills</a></li>
+                            <li><a onClick={props.scrollToTargetAdjusted} href="#Experience">Experience</a></li>
+                            <li><a  href={`${process.env.PUBLIC_URL}/Jalen Moorer Resume - 2020.pdf`} download>Resume</a></li>
+                            <li><a onClick={props.scrollToTargetAdjusted} href="mailto:jalenmmoorer@gmail.com">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Header/Header.stories.js
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from 'react';
+import Header from './Header';
+
+export default { title: 'Header' };
+
+export const HeaderNavigation = () => {
+    const [mobileNav, toggleMobileNav] = useState(false);
+
+    useEffect(() => {
+        function onResize(e) {
+          if (e.target.innerWidth >= 1024 && mobileNav)
+            toggleMobileNav(false);
+        }
+    
+        window.addEventListener("resize", onResize);
+    
+        return () => window.removeEventListener("resize", onResize);
+    
+      }, [mobileNav]);
+
+    function handleOpenMobileNav(e) {
+        toggleMobileNav(true);
+    }
+
+    function handleCloseMobileNav(e) {
+        toggleMobileNav(false);
+    }
+
+    function scrollToTargetAdjusted(e){
+        e.preventDefault();
+        const link = e.target.getAttribute('href')
+        const id = link.substring(1);
+        if (mobileNav)
+          toggleMobileNav(false);
+    
+        const element = document.getElementById(id)
+    
+        element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        window.history.pushState(null, null, link);
+    }
+
+    return (
+    <Header
+        onOpenMobileNav={handleOpenMobileNav}
+        oncloseMobileNav={handleCloseMobileNav}
+        mobileNav={mobileNav}
+        scrollToTargetAdjusted={scrollToTargetAdjusted}
+    />
+    );
+}
